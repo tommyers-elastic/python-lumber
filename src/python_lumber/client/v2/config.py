6b1ebce664f34e5596ab7f386b17d0d0d29f173e
tommyers-elastic <106530686+tommyers-elastic@users.noreply.github.com>
2022-08-17 23:38:49 +0100

    add default config options

--- src/python_lumber/client/v2/config.py
+++ src/python_lumber/client/v2/config.py
@@ -1,14 +1,12 @@
-from multiprocessing import connection
-
 class ConfigError(Exception): pass
 
 class ClientConfig:
     def __init__(
         self,
-        connection_timeout_s,
-        read_timeout_s,
-        write_timeout_s,
-        compression_level,
+        connection_timeout_s=None,
+        read_timeout_s=None,
+        write_timeout_s=None,
+        compression_level=0,
     ):
         self.connection_timeout_s = connection_timeout_s
         self.read_timeout_s = read_timeout_s 
@@ -16,13 +14,13 @@ class ClientConfig:
         self.compression_level = compression_level 
     
     def validate(self):
-        if self.connection_timeout_s <= 0:
+        if self.connection_timeout_s is not None and self.connection_timeout_s <= 0:
             raise ConfigError('connection timeout must be a positive number')
 
-        if self.read_timeout_s <= 0:
+        if self.read_timeout_s is not None and self.read_timeout_s <= 0:
             raise ConfigError('read timeout must be a positive number')
 
-        if self.write_timeout_s <= 0:
+        if self.write_timeout_s is not None and self.write_timeout_s <= 0:
             raise ConfigError('write timeout must be a positive number')
 
         if self.compression_level not in range(0, 10):
